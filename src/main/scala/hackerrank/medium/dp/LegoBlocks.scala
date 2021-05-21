ad68a3325d47edac2676af8bc11f7b26db4e2527
Harry Chen <chen.harry.hua@gmail.com>
2021-05-21 18:10:42 +1000

    hackerrankInString

--- src/main/scala/hackerrank/medium/dp/LegoBlocks.scala
+++ src/main/scala/hackerrank/medium/dp/LegoBlocks.scala
@@ -1,5 +1,7 @@
 package hackerrank.medium.dp
 
+import scala.collection.immutable.Queue
+
 // https://www.hackerrank.com/challenges/lego-blocks/problem
 object LegoBlocks {
 
@@ -7,5 +9,14 @@ object LegoBlocks {
     BigInt(23).pow(13)
     1
   }
+  Queue(1, 2).dequeue
+}
 
+object hackerrankInString {
+
+  def find(s: List[Char], stack: List[Char]): String = (s, stack) match {
+    case (a :: aa, b :: bb) => if (a == b) find(aa, bb) else find(aa, b :: bb)
+    case (Nil, b :: bb)     => "NO"
+    case (_, Nil)           => "YES"
+  }
 }
